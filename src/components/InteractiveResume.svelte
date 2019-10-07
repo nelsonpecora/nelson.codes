9edc07836f91050a9d50c962c6f7f455b9663259
Nelson Pecora <nelson@keats.me>
2019-10-06 21:54:55 -0400

    styled jobs

--- src/components/InteractiveResume.svelte
+++ src/components/InteractiveResume.svelte
@@ -1,5 +1,44 @@
 <script>
   import jobs from './jobs.json';
+  import Job from './Job.svelte';
+
+  let prevColor;
+  let companyColors = {};
+
+  function random (company) {
+    const colors = [
+      '#bbeaa6',
+      '#e3c878',
+      '#ed9a73',
+      '#e688a1',
+      '#c886e5',
+      '#99d8d0',
+      '#4baea0',
+      '#f299ee',
+      '#7189bf'
+    ];
+
+    if (companyColors[company]) {
+      return companyColors[company];
+    }
+
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    if (randomColor === prevColor) {
+      return random(company); // We just picked this color! pick another one
+    } else {
+      prevColor = randomColor;
+      companyColors[company] = randomColor;
+      return randomColor;
+    }
+  }
 </script>
 
-<p>resume</p>
+{#each jobs as job (job.company)}
+  <Job color={random(job.company)} {job} />
+  {#if job.subjobs}
+    {#each job.subjobs as subjob (subjob.company)}
+      <Job color={random(subjob.company)} job={subjob} />
+    {/each}
+  {/if}
+{/each}
---
+++ src/components/Job.svelte
@@ -0,0 +1,59 @@
+<script>
+  import Icon from '../../node_modules/fa-svelte'; // https://github.com/alphapeter/fa-svelte/issues/5
+  import { faAt } from '@fortawesome/free-solid-svg-icons';
+
+  export let color;
+  export let job;
+
+  const { start, end, place, role, company, desc } = job;
+</script>
+
+<style>
+  .job {
+    border-left: 8px solid grey;
+    padding-bottom: 20px;
+    padding-left: 20px;
+    position: relative;
+  }
+  .circle {
+    border-radius: 50%;
+    height: 30px;
+    left: -19px;
+    position: absolute;
+    top: -1px;
+    width: 30px;
+  }
+  .title {
+    align-items: center;
+    display: flex;
+    font-size: 20px;
+    justify-content: flex-start;
+    line-height: 20px;
+    margin: 0;
+  }
+  .icon {
+    font-size: 30px;
+    margin: 0 5px;
+  }
+  .subtitle {
+    color: #838586;
+    font-size: 14px;
+    margin: 0;
+  }
+  .desc {
+    margin: 10px 0 0;
+  }
+</style>
+
+<div class="job" style={`border-color: ${color};`}>
+  <div class="circle" style={`background-color: ${color};`} />
+  <p class="title">
+    {role}
+    <span class="icon" style={`color: ${color};`}>
+      <Icon icon={faAt} />
+    </span>
+    {company}
+  </p>
+  <p class="subtitle">{place} · <em>{start} – {end}</em></p>
+  <p class="desc">{desc}</p>
+</div>
